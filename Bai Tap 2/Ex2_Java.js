d1e8a374a8c242621bb1ae0673a4ed2d2399c7c5
Hoàng Phát Phạm <Hong Pht Phm>
2016-12-07 11:23:55 +0700

    Excercise HTML CSS

---
+++ Bai Tap 2/Ex2_Java.js
@@ -0,0 +1,53 @@
+﻿function Request(id_click){
+    div_click = document.getElementById(id_click);
+    var other1;
+    var other2;
+    var img_other1;
+    var img_other2;
+    switch(div_click.id){
+        case 'houseBunny':
+            div_click = document.getElementById('houseBunny');
+            other1 = document.getElementById('eagleEye');
+            other2 = document.getElementById('journey');
+            break;
+        case 'eagleEye':
+            div_click = document.getElementById('eagleEye');
+            other1 = document.getElementById('houseBunny');
+            other2 = document.getElementById('journey');
+            break;
+        case 'journey':
+            div_click = document.getElementById('journey');
+            other1 = document.getElementById('houseBunny');
+            other2 = document.getElementById('eagleEye');
+            break;
+    }
+
+    img_click = "img_" + div_click.id;
+    img_other1 = "img_" + other1.id;
+    img_other2 = "img_" + other2.id;
+
+    if (div_click.className == 'film_info_hidden'){ 
+ 
+        div_click.className = 'film_info_show';
+        document.getElementById(img_click).src = 'Image/ToBottom.png';
+        other1.className = 'film_info_hidden';
+        document.getElementById(img_other1).src = 'Image/ToRight.png';
+        other2.className = 'film_info_hidden';
+        document.getElementById(img_other2).src = 'Image/ToRight.png';
+    }
+}
+
+
+
+var img_id;
+
+function get_img_id(value){
+    img_id = document.getElementById(value).id;
+}
+
+function Vote1(score){
+    document.getElementById("vote_eagle_eyes").src = "Image/" + score + ".png"; 
+}
+function Vote2(score){
+    document.getElementById("vote_house").src = "Image/" + score + ".png"; 
+}
---
+++ Bai Tap 6/Image/Calendar.php
@@ -0,0 +1,4 @@
+<?php
+    <script type = "text/javascript" src = "Calendar.js"> </script>
+    LoadCalendar();
+?>
